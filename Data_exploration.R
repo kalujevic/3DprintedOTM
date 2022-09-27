ec45f574584cc855d3ba7e99bbbeff30b5d5c927
kalujevic <alujevick@gmail.com>
2022-09-27 15:38:35 -0700

    Preliminary plots with Sceloporus occidentalis

--- Data_exploration.R
+++ Data_exploration.R
@@ -1,1 +1,51 @@
-### Data exploration Logan lab ###
+##########################################################
+############## Data exploration Logan lab ################
+#################### September 2022 ######################
+##########################################################
+
+library(ggplot2)
+
+ggplot(data=Extracted_data,aes(x=Mass,y=MeanT)) + 
+  geom_point(aes(colour=OTM_type,shape=OTM_type),size=2) + 
+  geom_line(aes(colour=OTM_type))+
+  #stat_smooth(aes(colour=OTM_type),method='lm',linetype='dashed') +
+  facet_wrap(~Microsite)
+
+
+## plotting the relationship between lizard temperature and each of the models
+
+Extracted_data_restructured$Microsite <- ifelse(Extracted_data_restructured$Microsite == "rock_shade", 
+                                                "Rock Shade", "Rock Sun")
+
+plot1 <- ggplot(data=Extracted_data_restructured,aes(x=LizardT,y=MeanT)) + 
+            geom_point(aes(colour=OTM_type,shape=OTM_type),size=2) + 
+            stat_smooth(aes(colour=OTM_type,fill=OTM_type),method='lm',linetype='dashed', alpha=0.25) +
+            facet_wrap(~Microsite,scales='free') +
+            ggtitle(~bold("OTM validation")~bolditalic("Sceloporus occidentalis")) +
+            scale_x_continuous(name='Lizard Temperature (°C)') + 
+            scale_y_continuous(name='Model Temperature (°C)') + 
+            theme_bw() +
+            theme(plot.title = element_text(size = 14, colour="black",face = "bold",hjust = 0.5),
+                  axis.title = element_text(size=12, face = "bold"),
+                  legend.title = element_text(size = 12, colour="black",face = "bold",vjust = 1),
+                  legend.text = element_text(size=10),
+                  strip.text.x = element_text(size = 12, face = "bold"),
+                  panel.grid.minor = element_blank(),
+                  panel.grid.major = element_blank())
+
+plot1 + geom_abline(intercept = 0, slope = 1)
+
+ggsave("Sceloporus_occidentalis_validation.pdf", height = 6, width = 8)
+
+
+#plotting the variance around each model and lizard
+
+plot2 <- ggplot(data=Extracted_data,aes(x=OTM_type,y=SD)) + 
+            geom_boxplot(aes(fill=OTM_type)) + 
+            facet_wrap(~Microsite,scales='free') +
+            ggtitle('OTM validation Sceloporus occidentalis') +
+            scale_y_continuous(name='Standard deviation within equilibration') + 
+            theme(strip.text.x = element_text(size = 10))
+
+
+
