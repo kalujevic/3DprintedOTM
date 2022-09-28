c2fe281e40b13532c3cdd3bc97875657aa73bb37
kalujevic <alujevick@gmail.com>
2022-09-28 15:47:29 -0700

    Added Podarcis muralis

--- Data_exploration.R
+++ Data_exploration.R
@@ -6,6 +6,7 @@
 ################ Sceloporus occidentalis #################
 
 library(ggplot2)
+library(tidyverse)
 
 ggplot(data=Extracted_data,aes(x=Mass,y=MeanT)) + 
   geom_point(aes(colour=OTM_type,shape=OTM_type),size=2) + 
@@ -73,3 +74,26 @@ plot3 <- ggplot(data=Extracted_data,aes(x=OTM_type,y=SD)) +
 
 
 
+################ Podarcis muralis #################
+
+
+## plotting the relationship between lizard temperature and each of the models across microsites
+
+Extracted_data_restructured %>%
+  filter (Species=="Podarcis_muralis") %>%
+  ggplot(aes(x=LizardT,y=MeanT)) + 
+  geom_point(aes(colour=OTM_type,shape=Microsite),size=2) + 
+  stat_smooth(aes(colour=OTM_type,fill=OTM_type),method='lm',linetype='dashed', alpha=0.25) +
+  ggtitle(~bold("OTM validation")~bolditalic("Podarcis muralis")) +
+  scale_x_continuous(name='Lizard Temperature (°C)') + 
+  scale_y_continuous(name='Model Temperature (°C)') + 
+  theme_bw() +
+  theme(plot.title = element_text(size = 14, colour="black",face = "bold",hjust = 0.5),
+        axis.title = element_text(size=12, face = "bold"),
+        legend.title = element_text(size = 12, colour="black",face = "bold",vjust = 1),
+        legend.text = element_text(size=10),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank()) +
+  geom_abline(intercept = 0, slope = 1)
+  
+ggsave("Podarcis_muralis_validation_across_microsites.pdf", height = 6, width = 8)
