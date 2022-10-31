7ebd3e7b5d0b8ce6a6f5a27bdaf5eeafacf3d251
kalujevic <alujevick@gmail.com>
2022-10-31 13:29:32 -0700

    Generated plots for absolute difference in temp between lizards and models

--- Data_exploration.R
+++ Data_exploration.R
@@ -150,3 +150,126 @@ Extracted_data_restructured %>%
   geom_abline(intercept = 0, slope = 1)
 
 ggsave("Anolis_pulchellus_validation_across_microsites.pdf", height = 6, width = 8)
+
+
+
+################ Anolis sagrei #################
+
+
+## plotting the relationship between lizard temperature and each of the models across microsites
+
+Extracted_data_restructured %>%
+  filter (Species=="Anolis_sagrei") %>%
+  ggplot(aes(x=LizardT,y=MeanT)) + 
+  geom_point(aes(colour=OTM_type,shape=Microsite),size=2) + 
+  stat_smooth(aes(colour=OTM_type,fill=OTM_type),method='lm',linetype='dashed', alpha=0.25) +
+  ggtitle(~bold("OTM validation")~bolditalic("Anolis sagrei")) +
+  scale_x_continuous(name='Lizard Temperature (°C)') + 
+  scale_y_continuous(name='Model Temperature (°C)') + 
+  theme_bw() +
+  theme(plot.title = element_text(size = 14, colour="black",face = "bold",hjust = 0.5),
+        axis.title = element_text(size=12, face = "bold"),
+        legend.title = element_text(size = 12, colour="black",face = "bold",vjust = 1),
+        legend.text = element_text(size=10),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank()) +
+  geom_abline(intercept = 0, slope = 1)
+
+ggsave("Anolis_sagrei_validation_across_microsites.pdf", height = 6, width = 8)
+
+
+
+################ Anolis gundlachi #################
+
+
+## plotting the relationship between lizard temperature and each of the models across microsites
+
+Extracted_data_restructured %>%
+  filter (Species=="Anolis_cristatellus") %>%
+  ggplot(aes(x=LizardT,y=MeanT)) + 
+  geom_point(aes(colour=OTM_type,shape=Microsite),size=2) + 
+  stat_smooth(aes(colour=OTM_type,fill=OTM_type),method='lm',linetype='dashed', alpha=0.25) +
+  ggtitle(~bold("OTM validation")~bolditalic("Anolis cristatellus")) +
+  scale_x_continuous(name='Lizard Temperature (°C)') + 
+  scale_y_continuous(name='Model Temperature (°C)') + 
+  theme_bw() +
+  theme(plot.title = element_text(size = 14, colour="black",face = "bold",hjust = 0.5),
+        axis.title = element_text(size=12, face = "bold"),
+        legend.title = element_text(size = 12, colour="black",face = "bold",vjust = 1),
+        legend.text = element_text(size=10),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank()) +
+  geom_abline(intercept = 0, slope = 1)
+
+ggsave("Anolis_christatellus_validation_across_microsites.pdf", height = 6, width = 8)
+
+
+
+############### All species merged #################
+
+Extracted_data_restructured %>%
+  ggplot(aes(x=LizardT,y=MeanT)) + 
+  geom_point(aes(colour=OTM_type),size=2) + 
+  stat_smooth(aes(colour=OTM_type),method='lm',linetype='dashed', alpha=0.25) +
+  ggtitle(~bold("OTM validation all species pooled")) +
+  scale_x_continuous(name='Lizard Temperature (°C)') + 
+  scale_y_continuous(name='Model Temperature (°C)') + 
+  theme_bw() +
+  theme(plot.title = element_text(size = 14, colour="black",face = "bold",hjust = 0.5),
+        axis.title = element_text(size=12, face = "bold"),
+        legend.title = element_text(size = 12, colour="black",face = "bold",vjust = 1),
+        legend.text = element_text(size=10),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank()) +
+  geom_abline(intercept = 0, slope = 1)
+
+#should we discard this outlier at less than 10C?
+
+ggsave("All_species_validation_across_microsites.pdf", height = 6, width = 8)
+
+
+
+################### Absolute difference between lizard temp and each of the model temps #####################
+
+## across all species:
+
+Extracted_data_restructured %>%
+  ggplot(aes(x=OTM_type,y=Abs_differenceT)) + 
+  geom_boxplot(aes(fill=OTM_type),color='black', size=0.5) + 
+  ggtitle(~bold("Absolute difference between lizard and model temperatures")) +
+  scale_y_continuous(name='|ΔT| (°C)') + 
+  theme_bw() +
+  theme(plot.title = element_text(size = 14, colour="black",face = "bold",hjust = 0.5),
+        axis.title = element_text(size=12, face = "bold"),
+        legend.title = element_text(size = 12, colour="black",face = "bold",vjust = 1),
+        legend.text = element_text(size=10),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank())
+ 
+
+ggsave("Absolute_difference_between_lizard_model_temperatures_all.pdf", height = 6, width = 10)
+
+
+a<-lm(Abs_differenceT~OTM_type, data=Extracted_data_restructured)
+summary(a)
+
+## by species:
+
+Extracted_data_restructured %>%
+  ggplot(aes(x=OTM_type,y=Abs_differenceT)) + 
+  geom_boxplot(aes(fill=OTM_type),color='black', size=0.5) + 
+  ggtitle(~bold("Absolute difference between lizard and model temperatures")) +
+  scale_y_continuous(name='|ΔT| (°C)') + 
+  theme_bw() +
+  theme(plot.title = element_text(size = 14, colour="black",face = "bold",hjust = 0.5),
+        axis.title = element_text(size=12, face = "bold"),
+        legend.title = element_text(size = 12, colour="black",face = "bold",vjust = 1),
+        legend.text = element_text(size=10),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank()) +
+  facet_wrap(~ Species)
+
+
+ggsave("Absolute_difference_between_lizard_model_temperatures_per_species.pdf", height = 6, width = 11)
+
+
